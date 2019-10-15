84b79c1fe489210536366aadc6b4386f119910e3
ezhuravel <ezhuravel@gmail.com>
2019-10-14 21:08:52 -0400

    added classes and xml parser

--- Project.py
+++ Project.py
@@ -7,22 +7,41 @@ Created on Mon October 13 2019
 
 Group Project
 """
+from xml.dom import minidom
+import pandas as pd
 
+class Vector:
+    def __init__ (self, name):
+        self.name = name
+        self.edges = []
+        
 class Map:
-    def build_map(filename):
+    def build_map(self,filename):
         # Reads in the xml file and builds a map. Format we
         # are using is: https://graphonline.ru/en/
-        pass
+        
+        vectors = []
+        xmldoc = minidom.parse(filename)
+        itemlist = xmldoc.getElementsByTagName('node')
+        edgelist = xmldoc.getElementsByTagName('node')
+
+        print(itemlist.length)
+        #for i in itemlist:
+         #   print (i.attributes['id'].value)
+          #  vector = Vector(i.attributes['id'].value
+           # vectors.append())
     
-    def find_path():
+    
+    
+    def find_path(self):
         # takes exisiting map and runs Dijkstra's algorithn
         pass
     
-    def print_result():
+    def print_result(self):
         # prints out the path taken
         pass
         
-    def print_map():
+    def print_map(self):
         # This is optional, we can print the graph first, then show the path we take.
         pass
 
@@ -33,7 +52,7 @@ print("")
 
 
 map = Map()
-map.build_map("file") 
-map.print_map("file") # I think this can be optional
+map.build_map('map.graphml') 
+map.print_map() # I think this can be optional
 map.find_path()
 map.print_map()
