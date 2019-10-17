82b18f1e6d36789b6fb90e312ff7f523c8dfa4d7
ezhuravel <ezhuravel@gmail.com>
2019-10-16 23:27:47 -0400

    comlpleted dyksters algorithm

--- Project.py
+++ Project.py
@@ -8,7 +8,6 @@ Created on Mon October 13 2019
 Group Project
 """
 from xml.dom import minidom
-import numpy as np
 
 class Vector:
     def __init__ (self, name):
@@ -54,12 +53,48 @@ class Map:
             if edge.attributes['isDirect'].value != "true":
                 end_vect.neighbors[start_vect] = (start_vect,weight)
     
-    def find_path(self):
-        # takes exisiting map and runs Dijkstra's algorithn
-        pass
+    def find_path(self, start, target):
+        path ={start: (None, 0)}
+        current_node = self.vectors[start]
+        end_node = self.vectors[target]    
+        visited = set()
+        
+       
+        
+        while current_node != target:
+            visited.add(current_node)
+            weight_to_current_node = path[current_node][1]
+            
+            for neighbor in current_node.neighbors:
+                weight  = current_node.neighbors[neighbor][1] + weight_to_current_node
+                
+                if neighbor not in path:
+                        path[neighbor] = (current_node, weight)                
+                else:
+                    current_shortest_weight = path[neighbor][1]
+                    if current_shortest_weight > weight:
+                        path[neighbor] = (current_node, weight)
+            
+            next_destinations = {node: path[node] for node in path if node not in visited}
+            if not next_destinations:
+                return "Route Not Possible"
+            
+            current_node = min(next_destinations, key=lambda k: next_destinations[k][1])
+            # Work back through destinations in shortest path
+        
+        node_path = []
+        while current_node is not None:
+            node_path.append(current_node)
+            next_node = path[current_node][0]
+            current_node = next_node
+        # Reverse path
+        node_path = node_path[::-1]
+        
+        print(node_path)
+
     
     def print_result(self):
-        # prints out the path taken
+
         pass
         
     def print_map(self):
@@ -79,6 +114,5 @@ print("")
 
 map = Map()
 map.build_map('map.graphml') 
-map.print_map() # I think this can be optional
-map.find_path()
-map.print_map()
+#map.print_map() # I think this can be optional
+map.find_path('10', '4')
